52563cfd2082345e46ce5c06a97fe8c12f4f4793
Zemledelec <mgevlich@gmail.com>
2023-08-02 12:47:05 +0400

    WIP.0 Added og submodule

--- rays/rays.js
+++ rays/rays.js
@@ -1,81 +1,74 @@
-function main() {
-    var osm = new og.layer.XYZ("OpenStreetMap", {
-        isBaseLayer: true,
-        url: "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        visibility: true,
-        attribution: 'Data @ OpenStreetMap contributors, ODbL'
-    });
+import { Globe } from "../../src/og/Globe.js";
+import { XYZ } from "../../src/og/layer/XYZ.js";
+import { GlobusTerrain } from "../../src/og/terrain/GlobusTerrain.js";
+import { LonLat } from "../../src/og/LonLat.js";
+import { Vector } from "../../src/og/layer/Vector.js";
+import { Entity } from "../../src/og/entity/Entity.js";
+import { wgs84 } from "../../src/og/ellipsoid/wgs84.js";
 
-    //ellipsoid with earth dimensions
-    let ellipsoid = og.wgs84;
+var osm = new XYZ("OpenStreetMap", {
+    isBaseLayer: true,
+    url: "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    visibility: true,
+    attribution: "Data @ OpenStreetMap contributors, ODbL"
+});
 
-    //coordinates of Bochum in lonlat
-    let lonlatBochum = new og.LonLat(7, 51.5, 0);
-    //coordinate above Bochum to allow a upwards direction of ray
-    let lonlatBochumAir = new og.LonLat(7, 51.5, 2000000);
-    //coordinates of Bochum in Cartesian
-    let cartBochum = ellipsoid.lonLatToCartesian(lonlatBochum);
-    let cartBochumAir = ellipsoid.lonLatToCartesian(lonlatBochumAir);
-    //entity containing the Bochum ray
-    let entityBochum = new og.Entity({
-        'ray': {
-            'startPosition': cartBochum,
-            'endPosition': cartBochumAir,
-            'startColor': "blue",
-            'endColor': "green",
-            'thickness': 5
-        }
-    });
+//ellipsoid with earth dimensions
+let ellipsoid = wgs84;
 
-    //coordinates of Moscow in lonlat
-    let lonlatMoscow = new og.LonLat(37.6, 55.75, 0);
-    //coordinate above Moscow to allow a upwards direction of ray
-    let lonlatMoscowAir = new og.LonLat(37.6, 55.75, 1000000);
-    //coordinates of Moscow in Cartesian
-    let cartMoscow = ellipsoid.lonLatToCartesian(lonlatMoscow);
-    let cartMoscowAir = ellipsoid.lonLatToCartesian(lonlatMoscowAir);
-    //entity containing the Moscow ray
-    let entityMoscow = new og.Entity({
-        'ray': {
-            'startPosition': cartMoscow,
-            'endPosition': cartMoscowAir,
-            'startColor': "red",
-            'endColor': "green",
-            'thickness': 10
-        }
-    });
+//coordinates of Bochum in lonlat
+let lonlatBochum = new LonLat(7, 51.5, 0);
+//coordinate above Bochum to allow a upwards direction of ray
+let lonlatBochumAir = new LonLat(7, 51.5, 2000000);
+//coordinates of Bochum in Cartesian
+let cartBochum = ellipsoid.lonLatToCartesian(lonlatBochum);
+let cartBochumAir = ellipsoid.lonLatToCartesian(lonlatBochumAir);
+//entity containing the Bochum ray
+let entityBochum = new Entity({
+    ray: {
+        startPosition: cartBochum,
+        endPosition: cartBochumAir,
+        startColor: "blue",
+        endColor: "green",
+        thickness: 5
+    }
+});
 
-    //polygonOffsetUnits is needed to hide rays behind globe
-    let rayLayer = new og.layer.Vector("rays", { 'polygonOffsetUnits': 0 });
+//coordinates of Moscow in lonlat
+let lonlatMoscow = new LonLat(37.6, 55.75, 0);
+//coordinate above Moscow to allow a upwards direction of ray
+let lonlatMoscowAir = new LonLat(37.6, 55.75, 1000000);
+//coordinates of Moscow in Cartesian
+let cartMoscow = ellipsoid.lonLatToCartesian(lonlatMoscow);
+let cartMoscowAir = ellipsoid.lonLatToCartesian(lonlatMoscowAir);
+//entity containing the Moscow ray
+let entityMoscow = new Entity({
+    ray: {
+        startPosition: cartMoscow,
+        endPosition: cartMoscowAir,
+        startColor: "red",
+        endColor: "green",
+        thickness: 10
+    }
+});
 
-    //add entities containing the rays to the layer
-    rayLayer.add(entityBochum);
-    rayLayer.add(entityMoscow);
+//polygonOffsetUnits is needed to hide rays behind globe
+let rayLayer = new Vector("rays", { polygonOffsetUnits: 0 });
 
-    var globus = new og.Globe({
-        "target": "globus",
-        "name": "Earth",
-        "terrain": new og.terrain.GlobusTerrain(),
-        "layers": [osm, rayLayer],
-        "sun": {
-            "active": true
-        }
-    });
+//add entities containing the rays to the layer
+rayLayer.add(entityBochum);
+rayLayer.add(entityMoscow);
 
-    function createRay(startPos, endPos, length, startColor, endColor, thickness) {
+var globus = new Globe({
+    target: "globus",
+    name: "Earth",
+    terrain: new GlobusTerrain(),
+    layers: [osm, rayLayer],
+    sun: {
+        active: true
+    }
+});
 
-        let e = new og.Entity({
-            'ray': {
-                'startPosition': startPos,
-                'endPosition': endPos,
-                'length': length,
-                'startColor': "red",
-                'endColor': "green",
-                'thickness': 10
-            }
-        });
-        return e;
-    };
-
-    window.globus = globus;
-};
+window.globus = globus;
+window.entityMoscow = entityMoscow;
+window.entityBochum = entityBochum;
